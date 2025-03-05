a2c65a2b773a1c37da2aeb727b65140b0e24716b
HeitorSrDev <162715214+HeitorSchwRach@users.noreply.github.com>
2025-03-05 12:34:48 -0300

    Passo inicial e criação de variáveis

---
+++ cpf.js
@@ -0,0 +1,15 @@
+//Verificar se a var tem 11 digitos
+//Criar uma var para armazenar cpf
+//Separar cada numero do cpf em itens do array(replace, split)
+
+//criar uma var chamada somatorio
+//fazer um for loop que vai multiplicar cada num do var por 10 até 2 e somar na variavel somatorio
+//calcular o resto da divisao por 11 da var somatorio %
+//criar uma condicional para verificar se o digito é valido
+
+const cpf = [1,3,8,8,0,2,4,6,9,1,2]
+var somatorio = 0
+
+if(cpf.length != 11){
+    console.log('Não é um cpf')
+}else if()
